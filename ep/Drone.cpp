617d23a6ad46b15264146213330df5dca8a2ff11
b-andreoni <brunos-9@hotmail.com>
2023-07-14 12:13:43 -0300

    :sparkles: Drone

--- ep/Drone.cpp
+++ ep/Drone.cpp
@@ -1,11 +1,55 @@
 #include "Drone.h"
+#include <math.h>
 
 Drone::Drone()
 {
-
 }
 
 Drone::~Drone()
 {
+}
 
+float Drone::getEficiencia() {
+    return eficiencia;
+}
+void Drone::setEficiencia(float eficiencia){
+    this->eficiencia = eficiencia;
+}
+bool Drone::getDecolado(){
+    return decolado;
+};
+void Drone::setDecolado(bool decolado){
+    this->decolado = decolado;
+}
+int* Drone::getPosicao(){
+    return posicao;
+}
+void Drone::setPosicao(int posicao[2]){
+    this->posicao[2] = posicao[2];
+}
+
+bool Drone::Takeoff(){
+    if (!decolado && bateria.getCargaTotal()>0) {
+        decolado = true;
+        return true;
+    }
+    return false;
+}
+bool Drone::Pousar(){
+    if (decolado) {
+        decolado = false;
+        return true;
+    }
+    return false;
+}
+bool Drone::Voar (int x, int y){
+    int difX = x - posicao[0];
+    int difY = y - posicao[1];
+    int distancia = sqrt((pow(difX,2)) + pow(difY,2));
+    if (distancia*eficiencia > bateria.getCargaAtual()){
+        posicao[0] = x;
+        posicao[1] = y;
+        return true;
+    }
+    return false;
 }
--- ep/Drone.h
+++ ep/Drone.h
@@ -1,15 +1,37 @@
 #ifndef DRONE_H
 #define DRONE_H
+#include "Bateria.h"
 
 #pragma once
+using namespace std;
 
 class Drone
 {
 public:
+    Bateria bateria;
+    float getEficiencia();
+    void setEficiencia(float eficiencia);
+    bool getDecolado();
+    void setDecolado(bool decolado);
+    int* getPosicao();
+    void setPosicao(int posicao[2]);
+
+
+    bool Takeoff();
+    bool Pousar();
+    bool Voar (int x, int y);
+private:
     Drone();
     ~Drone();
 
-private:
+
+    int posicao[2];
+    float eficiencia;
+    bool decolado;
+
+    bool Takeoff();
+    bool Pousar();
+    bool Voar (int x, int y);
 
 };
 
